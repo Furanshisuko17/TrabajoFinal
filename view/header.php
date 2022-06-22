0b69b80a6fce9d46c014f20c046e3d8ca50f735b
Furanshisuko17 <darkfran555@gmail.com>
2022-06-22 10:00:52 -0500

    Update header.php

--- view/header.php
+++ view/header.php
@@ -8,13 +8,35 @@
     <a class="name" href="../index.php" title="Computadoras">
         <strong>Computadoras</strong>
     </a>
+    
+    <?php if($include_option == 'contactanos') { ?>
+        <a class="selected-page" href="#" title="Cont&aacute;ctanos">Cont&aacute;ctanos</a>
+    <?php } else { ?>
+        <a href="../contactanos.php" title="Cont&aacute;ctanos">Cont&aacute;ctanos</a>
+    <?php } ?>
 
+    <?php if($include_option == 'productos') { ?>
+        <a class="selected-page" href="#" title="Productos que ofrecemos">Productos</a> 
+    <?php } else { ?>
+        <a href="../productos.php" title="Productos que ofrecemos">Productos</a> 
+    <?php } ?>
 
+    <?php if($include_option == 'servicios') { ?>
+        <a class="selected-page" href="#" title="Nuestros servicios">Servicios</a>
+    <?php } else { ?>
+        <a href="../servicios.php" title="Nuestros servicios">Servicios</a>
+    <?php } ?>
 
-    <a href="../contactanos.php" title="Cont&aacute;ctanos">Cont&aacute;ctanos</a>
-    <a href="../productos.php" title="Productos que ofrecemos">Productos</a> 
-    <a href="../servicios.php" title="Nuestros servicios">Servicios</a>
-    <a href="../nosotros.php" title="Acerca de nosotros">Nosotros</a>
-    <a href="../index.php" title="Inicio">Inicio</a>
+    <?php if($include_option == 'nosotros') { ?>
+        <a class="selected-page" href="#" title="Acerca de nosotros">Nosotros</a>
+    <?php } else { ?>
+        <a href="../nosotros.php" title="Acerca de nosotros">Nosotros</a>
+    <?php } ?>
+
+    <?php if($include_option == 'index') { ?>
+        <a class="selected-page" href="#" title="Inicio">Inicio</a>
+    <?php } else { ?>
+        <a href="../index.php" title="Inicio">Inicio</a>
+    <?php } ?>    
 
 </div>
