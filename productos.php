a004cc94e5fbdfce52c291979767bbec8420e304
Furanshisuko17 <darkfran555@gmail.com>
2022-06-28 19:32:54 -0500

    added connection to database

--- productos.php
+++ productos.php
@@ -31,7 +31,7 @@
                 <a onclick="changeUrl('../productos/links/psu.html')" id="psu" href="javascript:void(0);">Fuentes de poder</a>
                 <a onclick="changeUrl('../productos/links/graphiccard.html')" id="graphiccard" href="javascript:void(0);">Tarjetas gr&aacute;ficas</a>
                 <a onclick="changeUrl('../productos/links/motherboard.html')" id="motherboard" href="javascript:void(0);">Tarjetas madre</a>
-                <a onclick="changeUrl('../productos/links/processor.html')" id="processors" href="javascript:void(0);">Procesadores</a>
+                <a onclick="changeUrl('../productos/links/processor.php')" id="processors" href="javascript:void(0);">Procesadores</a>
         
             </div>
             <iframe seamless name="main-content" height="100%" width="100%" src="../productos/main.html" ></iframe>
@@ -41,3 +41,23 @@
         </div> 
     </body>
 </html>
+
+
+<!-- <div class="content" >
+
+    <div class="productbar">
+
+        <a onclick="changeUrl('../productos/main.html')" id="main" style="padding-top:11px;float:left;font-size:120%" href="javascript:void(0);">Inicio</a>
+
+        <a onclick="changeUrl('../productos/links/case.html')" id="case" href="javascript:void(0);">Cajas</a>
+        <a onclick="changeUrl('../productos/links/psu.html')" id="psu" href="javascript:void(0);">Fuentes de poder</a>
+        <a onclick="changeUrl('../productos/links/graphiccard.html')" id="graphiccard" href="javascript:void(0);">Tarjetas gr&aacute;ficas</a>
+        <a onclick="changeUrl('../productos/links/motherboard.html')" id="motherboard" href="javascript:void(0);">Tarjetas madre</a>
+        <a onclick="changeUrl('../productos/links/processor.html')" id="processors" href="javascript:void(0);">Procesadores</a>
+
+    </div>
+    <iframe seamless name="main-content" height="100%" width="100%" src="../productos/main.html" ></iframe>
+    <?php
+        //include('view/footer.php');
+    ?>
+</div>  -->
---
+++ productos/links/processor.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/content.css">
+    <link rel="stylesheet" href="../css/table.css">
+
+</head>
+<body>
+    <div class="content">
+        <div class="sidebar">
+            <p>Sidebar content</p>
+        </div>
+
+        
+        <div class="table main">
+            <table aria-label="table processor content">
+                <colgroup>
+                    <col width="30%">
+                    <col width="14%">
+                    <col width="14%">
+                    <col width="12%">
+                    <col width="14%">
+                    <col width="14%">
+                </colgroup>
+                <thead>
+                    <tr> 
+                        <th>Nombre</th>
+                        <th>N&uacute;cleos</th>
+                        <th>Velocidad</th>
+                        <th>TDP</th>
+                        <th>Gr&aacute;ficos integrados</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="table-content">
+                                <div class="image-wrapper">
+                                    <img alt="Procesador Ryzen 5 5600X" src="img/processors/amd5600.jpg">
+                                </div>
+                                <div class="text name">AMD Ryzen 5 5600X</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table-content">
+                                <div class="text ">6</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table-content">
+                                <div class="text">3.7 Ghz</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table-content">
+                                <div class="text">65 W</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table-content">
+                                <div class="text">No tiene</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table-content">
+                                <div class="text">S/.796.95</div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
