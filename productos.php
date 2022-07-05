cff104e678c8cbc5e23e23e567b6bdd5f74486bf
Furanshisuko17 <darkfran555@gmail.com>
2022-07-05 00:48:35 -0500

    updated

--- productos.php
+++ productos.php
@@ -27,11 +27,11 @@
 
                 <a onclick="changeUrl('../products/main.html')" id="main" style="padding-top:11px;float:left;font-size:120%;padding-bottom:6.5px;" href="javascript:void(0);">Inicio</a>
         
-                <a onclick="changeUrl('../products/links/case.html')" id="case" href="javascript:void(0);">Cajas</a>
-                <a onclick="changeUrl('../products/links/psu.html')" id="psu" href="javascript:void(0);">Fuentes de poder</a>
-                <a onclick="changeUrl('../products/links/graphiccard.html')" id="graphiccard" href="javascript:void(0);">Tarjetas gr&aacute;ficas</a>
-                <a onclick="changeUrl('../products/links/motherboard.html')" id="motherboard" href="javascript:void(0);">Tarjetas madre</a>
-                <a onclick="changeUrl('../products/links/processor.php')" id="processors" href="javascript:void(0);">Procesadores</a>
+                <a onclick="changeUrl('../products/case.html')" id="case" href="javascript:void(0);">Cajas</a>
+                <a onclick="changeUrl('../products/psu.html')" id="psu" href="javascript:void(0);">Fuentes de poder</a>
+                <a onclick="changeUrl('../products/graphiccard.html')" id="graphiccard" href="javascript:void(0);">Tarjetas gr&aacute;ficas</a>
+                <a onclick="changeUrl('../products/motherboard.html')" id="motherboard" href="javascript:void(0);">Tarjetas madre</a>
+                <a onclick="changeUrl('../products/processor.php')" id="processors" href="javascript:void(0);">Procesadores</a>
         
             </div>
             <iframe seamless name="main-content" height="100%" width="100%" src="../products/main.html" ></iframe>
