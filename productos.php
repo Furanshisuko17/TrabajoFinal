e5376f28ae8f5eaa8ba4d1f254f1cf1bae8f1586
Furanshisuko17 <darkfran555@gmail.com>
2022-07-05 19:54:22 -0500

    revamped product bar with flexbox

--- productos.php
+++ productos.php
@@ -25,13 +25,13 @@
 
             <div class="productbar">
 
-                <a onclick="changeUrl('../products/main.html')" id="main" style="padding-top:11px;float:left;font-size:120%;padding-bottom:6.5px;" href="javascript:void(0);">Inicio</a>
+                <a class="inicio" id="main" href="">Inicio</a>
         
-                <a onclick="changeUrl('../products/case.html')" id="case" href="javascript:void(0);">Cajas</a>
-                <a onclick="changeUrl('../products/psu.html')" id="psu" href="javascript:void(0);">Fuentes de poder</a>
-                <a onclick="changeUrl('../products/graphiccard.html')" id="graphiccard" href="javascript:void(0);">Tarjetas gr&aacute;ficas</a>
-                <a onclick="changeUrl('../products/motherboard.html')" id="motherboard" href="javascript:void(0);">Tarjetas madre</a>
-                <a onclick="changeUrl('../products/processor.php')" id="processors" href="javascript:void(0);">Procesadores</a>
+                <a id="processors" href="">Procesadores</a>
+                <a id="motherboard" href="">Tarjetas madre</a>
+                <a id="graphiccard" href="">Tarjetas gr&aacute;ficas</a>
+                <a id="psu" href="">Fuentes de poder</a>
+                <a id="case" href="">Cajas</a>
         
             </div>
             <iframe seamless name="main-content" height="100%" width="100%" src="../products/main.html" ></iframe>
@@ -47,13 +47,13 @@
 
     <div class="productbar">
 
-        <a onclick="changeUrl('../productos/main.html')" id="main" style="padding-top:11px;float:left;font-size:120%" href="javascript:void(0);">Inicio</a>
+        <a onclick="changeUrl('../productos/main.html')" id="main" style="padding-top:11px;float:left;font-size:120%" href="">Inicio</a>
 
-        <a onclick="changeUrl('../productos/links/case.html')" id="case" href="javascript:void(0);">Cajas</a>
-        <a onclick="changeUrl('../productos/links/psu.html')" id="psu" href="javascript:void(0);">Fuentes de poder</a>
-        <a onclick="changeUrl('../productos/links/graphiccard.html')" id="graphiccard" href="javascript:void(0);">Tarjetas gr&aacute;ficas</a>
-        <a onclick="changeUrl('../productos/links/motherboard.html')" id="motherboard" href="javascript:void(0);">Tarjetas madre</a>
-        <a onclick="changeUrl('../productos/links/processor.html')" id="processors" href="javascript:void(0);">Procesadores</a>
+        <a onclick="changeUrl('../productos/links/case.html')" id="case" href="">Cajas</a>
+        <a onclick="changeUrl('../productos/links/psu.html')" id="psu" href="">Fuentes de poder</a>
+        <a onclick="changeUrl('../productos/links/graphiccard.html')" id="graphiccard" href="">Tarjetas gr&aacute;ficas</a>
+        <a onclick="changeUrl('../productos/links/motherboard.html')" id="motherboard" href="">Tarjetas madre</a>
+        <a onclick="changeUrl('../productos/links/processor.html')" id="processors" href="">Procesadores</a>
 
     </div>
     <iframe seamless name="main-content" height="100%" width="100%" src="../productos/main.html" ></iframe>
